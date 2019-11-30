ab1b3546ddf94a7e685ebf8746cf8b45fe3acf1d
Sumukh M Lohit <43979295+sumukhmlohit@users.noreply.github.com>
2019-11-30 19:09:59 +0530

    Update lbucket.c

--- part a/lbucket.c
+++ part a/lbucket.c
@@ -4,42 +4,91 @@ int queue[200];
 int bcap=0,orate=0,iter=0;
 int stop=1,idata,bdata=0;
 
+int iterations[200],incoming[200],bucket[200],outgoing[200],dropped[200];
 
+int drop;
 void main()
 {
-printf("Enter bucket capacity");
+printf("Enter bucket capacity\n");
 scanf("%d",&bcap);
 
-printf("Enter output rate");
+printf("Enter output rate\n");
 scanf("%d",&orate);
 
 
 while(stop!=0)
 { iter++;
-printf("Enter incoming data");
+drop=0;
+printf("Enter incoming data\n");
 scanf("%d",&idata);
 
-if(idata<orate)
+if(idata+bdata<orate)
 {
-printf("%d has been processed",idata);
+printf("%d has been processed\n",idata);
+bdata=bdata+idata-orate;
+
 }
 
 else if(idata+bdata-orate<=bcap)
 {
-printf("%d has been processed",orate);
+printf("%d",bdata);
+printf("%d has been processed\n",orate);
 bdata=idata+bdata-orate;
 }
 
 else
 {
-printf("%d has been processed",orate);
+    int bdata0=bdata;
+printf("%d has been processed\n",orate);
 bdata=bcap;
-printf("%d has been dropped",bdata+idata-orate-bcap);
+printf("%d has been dropped\n",bdata0+idata-orate-bcap);
+drop=bdata0+idata-orate-bcap;
 }
-printf("Bucket data is %d",bdata);
+printf("Bucket data is %d\n",bdata);
+
+iterations[iter]=iter;
+incoming[iter]=idata;
+outgoing[iter]=orate;
+bucket[iter]=bdata;
+dropped[iter]=drop;
+int i=0;
+
+printf("Iterations Incoming_Data Outgoing_Data Bucket_Data Data_dropped \n");
+for(i=1;i<=iter;i++)
+{   
+    printf("%d            %d             %d              %d            %d\n",iterations[i],incoming[i],outgoing[i],bucket[i],dropped[i]);
+    
+}
+
 printf("Enter 0 to stop");
 scanf("%d",&stop);
 }
 
+while(bdata!=0)
+{
+    iter++;
+     iterations[iter]=iter;
+    if(bdata>=5)
+    {bdata=bdata-orate;
+    printf("%d has been processed\n",orate);
+    }
+    else
+    {
+        printf("%d has been processed\n",bdata);
+    bdata=0;
+    }
+ int i;
+ iterations[iter]=iter;
+incoming[iter]=idata;
+outgoing[iter]=orate;
+bucket[iter]=bdata;
+dropped[iter]=drop;
+    printf("Iterations Incoming_Data Outgoing_Data Bucket_Data Data_dropped \n");
+for(i=1;i<=iter;i++)
+{   
+    printf("%d            %d             %d              %d            %d\n",iterations[i],incoming[i],outgoing[i],bucket[i],dropped[i]);
+    
+}       
+}
 
 }
